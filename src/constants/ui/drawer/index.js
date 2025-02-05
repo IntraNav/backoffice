6d992d98a5524e9b15950d1d0c3ef7a0af168c64
Johannes Tiyasi <theanthem8@gmail.com>
2025-02-05 20:31:50 +0200

    Create drawer constants

---
+++ src/constants/ui/drawer/index.js
@@ -0,0 +1,15 @@
+import { Dashboard, Message } from "@mui/icons-material";
+
+export const drawerRoutes = [
+  {
+    title: "Dashboard",
+    path: "/",
+    icon: <Dashboard fontSize="12px" />,
+  },
+
+  {
+    title: "Dummy",
+    path: "/dummy",
+    icon: <Message fontSize="12px" />,
+  },
+];
