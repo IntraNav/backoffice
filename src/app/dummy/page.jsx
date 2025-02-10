21ee22bade62b78ee490e90a03aa39fd1d69f1e8
Johannes Tiyasi <theanthem8@gmail.com>
2025-02-10 12:29:28 +0200

    Setup on mounted

--- src/app/dummy/page.jsx
+++ src/app/dummy/page.jsx
@@ -1,9 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppContext } from "@/context/app";
+import { usePathname } from "next/navigation";
+import { Box, Typography } from "@mui/material";
 
-const DummyPage = () => {
-  return <div>DummyPage</div>;
+const DummyPage = ({ props }) => {
+  const { setLoading, mounted } = useAppContext();
+  const pathname = usePathname();
+
+  console.log("props", props);
+
+  useEffect(() => {
+    setLoading(!mounted);
+  }, [mounted, setLoading]);
+
+  return (
+    <Box>
+      <Typography></Typography>
+    </Box>
+  );
 };
 
 export default DummyPage;
