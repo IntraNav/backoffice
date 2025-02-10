bd0e3db6f48c150dffafc91ddeed553d5ae4ec54
Johannes Tiyasi <theanthem8@gmail.com>
2025-02-10 12:26:36 +0200

    Add auth high order component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 "use client";
 
 import { useAppContext } from "@/context/app";
+import { withAccessControl } from "@/hoc/auth";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const { setLoading, mounted } = useAppContext();
+  const pathname = usePathname();
+  console.log("props", props);
+
+  useEffect(() => {
+    setLoading(!mounted);
+  }, [mounted, setLoading]);
+
   return (
     <Box sx={{ width: "100%" }}>
       <Typography>Home Page</Typography>
@@ -12,4 +23,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default withAccessControl(HomePage, []);
